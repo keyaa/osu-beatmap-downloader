cf7b322b557847e06f946f1aaaad4a947419bd3d
Kevin Yang <keya5541@colorado.edu>
2018-05-14 16:26:58 -0700

    added ignore feature for already downloaded maps

--- downloader.py
+++ downloader.py
@@ -2,10 +2,12 @@ import requests # needed to make a request to get beatmaps
 import re # needed for regex
 from bs4 import BeautifulSoup # needed for formatting
 import urllib.request # for downloading the actual beatmaps
-import webbrowser
+import webbrowser # needed to open links
+from os import listdir # needed to list current beatmaps
 
-# needed for auto open osz
+# needed for auto open osz files in firefox
 # https://addons.mozilla.org/en-US/firefox/addon/inlinedisposition-reloaded/
+# alternatively, chrome's "always open files of this type" works well.
 
 link = 'https://osu.ppy.sh/beatmapsets'
 r = requests.get(link)
@@ -19,6 +21,12 @@ maps = list(set(maps)) # get rid of duplicates (different difficulties are the s
 ids = []
 for i in maps: # create a list of the map ids
     ids.append(''.join(re.findall("[0-9]*", i)))
+
+current_songs = listdir("D:\Games\osu!\Songs") # list of already downloaded songs (hardcoded file path)
+for old_map in current_songs: # get rid of new beatmap ids that are already downloaded
+    for new_map in ids: # look through new maps
+        if new_map in old_map: # if it's already in user library
+            ids.remove(new_map) # delete it
 
 # converts the list of ids to a list of download links
 links = []
